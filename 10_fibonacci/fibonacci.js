e6219bd9813f4d7ccfad600f7e328cd1caf7f45f
Lin-DOS <26785782+Lin-DOS@users.noreply.github.com>
2024-09-12 17:49:10 +1000

    Complete personal solution

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,7 +1,20 @@
-const fibonacci = function() {
-}
+const fibonacci = function(nthTerm) {
+  const arr = [0, 1];
+
+  if (typeof nthTerm === "string") {
+    nthTerm = parseFloat(nthTerm);
+  }
+
+  if (nthTerm < 0 || !Number.isInteger(nthTerm)) {
+    return "OOPS";
+  }
+
+  for (let n = 2; n <= nthTerm; n++) {
+    arr[n] = arr[n - 1] + arr[n - 2]; 
+  }
+
+  return arr[nthTerm];
+};
 
 // Do not edit below this line
 module.exports = fibonacci;
-fibonacci(4); // returns the 4th member of the series: 3  (1, 1, 2, 3)
-fibonacci(6); // returns 8
