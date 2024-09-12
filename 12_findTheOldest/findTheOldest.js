55b51a855f6b0ca4ac6d21dee2da5b0bc0665e46
Lin-DOS <26785782+Lin-DOS@users.noreply.github.com>
2024-09-12 17:44:04 +1000

    Complete personal solution

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
+  
+  return arr.reduce((acc, element) => {
+    const currentDate = new Date().getUTCFullYear();
 
+    const oldest = !acc.yearOfDeath ? currentDate - acc.yearOfBirth : 
+                    acc.yearOfDeath - acc.yearOfBirth;
+
+    const currentValue = !element.yearOfDeath ? currentDate - element.yearOfBirth :
+                          element.yearOfDeath - element.yearOfBirth;
+    
+    return currentValue > oldest ? element : acc;
+  });
 };
 
 // Do not edit below this line
